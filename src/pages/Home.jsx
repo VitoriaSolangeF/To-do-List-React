ad8aa84f48cade58cb9d718f93b1e103d7715400
Vitória Solange <you@example.com>
2023-12-07 17:04:08 -0300

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,27 +4,41 @@ import Form from "../components/Form";
 import TodoItem from "../components/TodoItem";
 
 export default function Home() {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+  };
 
-    const addTodo = (todo) => {
-        setTodos([...todos, todo]);
+  const deleteTodo = (id) => {
+    var filtered = todos.filter((todo) => todo.id !== id);
+    setTodos(filtered);
+  };
+
+  const editTodo = (id, editedText) => {
+    var todosArray = todos;
+
+    for (var i in todosArray) {
+      if (todosArray[i].id == id) {
+        todosArray[i].text = editedText;
+      }
     }
 
-    const deleteTodo = (id) => {
-        var filtered = todos.filter((todo) => todo.id !== id);
-        setTodos(filtered);
-    }
+    // console.log(todosArray);
+    // todosArray.splice(todosArray.id, 1, { text: editedText, id: id });
+    // console.log(todosArray);
+    setTodos(todosArray);
+  };
 
-    return (
-        <Container maxWidth="xs" style={{ marginTop: "2em"}}>
-            <Form addTodo={addTodo} />
-            <List sx={{marginTop: "1em"}}>
-                {todos.map((todo) => (
-                    <div key={todo.id} style={{ marginTop: "1em" }}>
-                        <TodoItem todo={todo} deleteTodo={deleteTodo} />
-                    </div>
-                ))}
-            </List>
-        </Container>
-    );
+  return (
+    <Container maxWidth="xs" style={{ marginTop: "1em" }}>
+      <Form addTodo={addTodo} />
+      <List sx={{ marginTop: "1em" }}>
+        {todos.map((todo) => (
+          <div key={todo.id} style={{ marginTop: "1em" }}>
+            <TodoItem editTodo={editTodo} todo={todo} deleteTodo={deleteTodo} />
+          </div>
+        ))}
+      </List>
+    </Container>
+  );
 }
